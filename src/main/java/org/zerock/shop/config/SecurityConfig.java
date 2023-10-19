f6bcfa66e2185383c7bf0b09bb82c052b60e0d7e
ChoiSungho8 <csh2572@gmail.com>
2023-10-19 12:42:23 +0900

    ...

--- src/main/java/org/zerock/shop/config/SecurityConfig.java
+++ src/main/java/org/zerock/shop/config/SecurityConfig.java
@@ -24,17 +24,18 @@ public class SecurityConfig {
     }
 
     // static 디렉터리의 하위 파일은 인증을 무시하도록 설정
-    /*@Bean
+    @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring()
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**"))
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/v3/api-docs/**"))
+                .requestMatchers(AntPathRequestMatcher.antMatcher("/swagger*/**"))
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/replies/**"))
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/css/**"))
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/js/**"))
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/images/**"))
                 .requestMatchers(AntPathRequestMatcher.antMatcher("/img/**"));
-    }*/
+    }
 
     // http 요청에 대한 보안을 설정합니다.
     // 페이지 권한 설정, 로그인 페이지 설정, 로그아웃 메소드 등에 대한 설정 작성
@@ -51,18 +52,9 @@ public class SecurityConfig {
                         .logoutRequestMatcher(AntPathRequestMatcher.antMatcher("/members/logout"))
                         .logoutSuccessUrl("/"))
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/swagger*/**")).permitAll()
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/replies/**")).permitAll()
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/css/**")).permitAll()
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/js/**")).permitAll()
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/images/**")).permitAll()
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/swagger*/**")).permitAll()
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/h2-console/**")).permitAll()
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/img/**")).permitAll()
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/")).permitAll()
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/v3/api-docs/**")).permitAll()
+                        .requestMatchers(AntPathRequestMatcher.antMatcher("/member/**")).permitAll()
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/board/**")).permitAll()
-                        .requestMatchers(AntPathRequestMatcher.antMatcher("/members/**")).permitAll()
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/item/**")).permitAll()
                         .requestMatchers(AntPathRequestMatcher.antMatcher("/admin/**")).hasRole("ADMIN")
                         .anyRequest().authenticated())
--- src/main/java/org/zerock/shop/controller/ReplyController.java
+++ src/main/java/org/zerock/shop/controller/ReplyController.java
@@ -8,10 +8,9 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.zerock.shop.dto.PageRequestDto;
+import org.zerock.shop.dto.PageResponseDto;
 import org.zerock.shop.dto.ReplyDto;
 import org.zerock.shop.service.ReplyService;
 
@@ -44,6 +43,17 @@ public class ReplyController {
         resultMap.put("rno", rno);
 
         return resultMap;
+
+    }
+
+    // 특정 게시물 댓글 목록 처리
+    @Operation(summary = "Replies of Board", description = "GET 방식으로 특정 게시물의 댓글 목록")
+    @GetMapping(value = "/list/{bno}")
+    public PageResponseDto<ReplyDto> getList(@PathVariable("bno") Long bno, PageRequestDto pageRequestDto) {
+
+        PageResponseDto<ReplyDto> responseDto = replyService.getListOfBoard(bno, pageRequestDto);
+
+        return responseDto;
 
     }
 
