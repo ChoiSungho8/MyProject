d22e77ba11382faf2646b924839689dd8bfac115
ChoiSungho8 <csh2572@gmail.com>
2023-10-20 15:39:02 +0900

    p. 579

--- src/main/java/org/zerock/shop/repository/BoardRepository.java
+++ src/main/java/org/zerock/shop/repository/BoardRepository.java
@@ -1,11 +1,18 @@
 package org.zerock.shop.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.zerock.shop.entity.Board;
 import org.zerock.shop.repository.search.BoardSearch;
 
 public interface BoardRepository extends JpaRepository<Board, Long>, BoardSearch {
+
+    Page<Board> findByTitleContainingOrderByBnoDesc(String keyword, Pageable pageable);
+
+    @Query("select b from Board b where b.title like concat('%',:keyword,'%')")
+    Page<Board> findKeyword(String keyword, Pageable pageable);
 
     @Query(value = "select now()", nativeQuery = true)
     String getTime();
